fcbbec248339e545564064a7e64802de1e638ee5
codfish81 <codfish81@byui.edu>
2023-05-17 10:43:15 -0600

    added teams to index.js and updated swagger.json file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 routes.use('/', require('./swagger'))
 routes.use('/players', require('./contacts'));
+routes.use('/teams', require('./contacts'));
 // routes.use(
 //     '/',
 //     (docData = (req, res) => {
