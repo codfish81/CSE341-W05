ab9813d29a182cf2df1f7f937cb104246e88222b
codfish81 <codfish81@byui.edu>
2023-05-20 20:05:29 -0600

    cleaned up code a bit

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,5 @@ const routes = express.Router();
 routes.use('/', require('./swagger'))
 routes.use('/players', require('./contacts'));
 routes.use('/teams', require('./contacts'));
-// routes.use(
-//     '/',
-//     (docData = (req, res) => {
-//       let docData = {
-//         documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
-//       };
-//       res.send(docData);
-//     })
-//   );
 
 module.exports = routes;
