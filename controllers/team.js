dbe6b3c1c087814d9664bb1ccfcbf24c15a3069a
codfish81 <codfish81@byui.edu>
2023-05-23 19:09:44 -0600

    add test try catch block around getAllTeams

--- controllers/team.js
+++ controllers/team.js
@@ -1,12 +1,17 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+
 const getAllTeams = async (req, res, next) => {
+  try{
   const result = await mongodb.getDb().db().collection('teams').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
+}catch(err){
+  res.status(500).json(err);
+}
 };
   // mongodb
   //   .getDb()
